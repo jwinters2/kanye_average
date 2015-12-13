01b05f215a2fbc960b0950d8a5bcf830a0f80644
Alarindris <erik.sikich@gmail.com>
2015-12-13 09:02:04 -0600

    Formatting

--- kanye.cpp
+++ kanye.cpp
@@ -10,10 +10,10 @@ int main(int argc, char* argv[])
 	if(argc! == 3 || a == 0 || b == 0){
 		cout << "Invalid, use 2 non-zero numbers." << endl;
 		if(argc! == 3) ? return -1: return -2;
-    }
+	}
 	
 	float k = 0.25 *((a + b)+(a * b)+(a / b)+(b / a));
 	cout << "Kanye avg. = " << k << endl;
 	
-  return 0;
+	return 0;
 }
