86509bdf866422189ad3713e3d15d3d5ee669b08
Alarindris <erik.sikich@gmail.com>
2015-12-13 08:53:59 -0600

    Better use of variables.

--- kanye.cpp
+++ kanye.cpp
@@ -4,13 +4,15 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-	if(argc! == 3 || atof(argv[1]) == 0 || atof(argv[2]) == 0){
+	float a = atof(argv[1]);
+	float b = atof(argv[2]);
+	
+	if(argc! == 3 || a == 0 || b == 0){
 		cout << "Invalid, use 2 non-zero numbers." << endl;
     }
 	else{
-		float a = atof(argv[1]);
-		float b = atof(argv[2]);
-		cout << "Kanye avg. = " << 0.25 *((a + b)+(a * b)+(a / b)+(b / a)) << endl;
+		float k = 0.25 *((a + b)+(a * b)+(a / b)+(b / a));
+		cout << "Kanye avg. = " << k << endl;
 	}
 	
   return 0;
